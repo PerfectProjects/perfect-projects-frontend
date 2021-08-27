cb57cc63975cd2310921b6504abca9ad1eccd01f
Oskar <reoskaro434@gmail.com>
2021-08-27 17:16:46 +0200

    Added a few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main/header/header.component';
+import { BodyComponent } from './main/body/body.component';
+import { ProjectListComponent } from './main/body/project-list/project-list.component';
+import { ProjectComponent } from './main/body/project-list/project/project.component';
+import { ProjectThumbnailComponent } from './main/body/project-list/project/project-thumbnail/project-thumbnail.component';
+import { ProjectDescriptionComponent } from './main/body/project-list/project/project-description/project-description.component';
+import { ProjectStatsComponent } from './main/body/project-list/project/project-stats/project-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    ProjectThumbnailComponent,
+    ProjectDescriptionComponent,
+    ProjectStatsComponent
   ],
   imports: [
     BrowserModule,
