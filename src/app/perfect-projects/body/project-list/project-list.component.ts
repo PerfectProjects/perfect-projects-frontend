ac620af3fe59f9cffe4f23a6f0dc61b288c415a2
Oskar <reoskaro434@gmail.com>
2021-09-23 20:02:28 +0200

    created static project list

--- src/app/perfect-projects/body/project-list/project-list.component.ts
+++ src/app/perfect-projects/body/project-list/project-list.component.ts
@@ -7,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProjectListComponent implements OnInit {
 
+  pictures = ["game", "picture", "room"];
+
   constructor() {
   }
 
--- src/app/perfect-projects/body/project-list/project/project-thumbnail/project-thumbnail.component.ts
+++ src/app/perfect-projects/body/project-list/project/project-thumbnail/project-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project-thumbnail',
@@ -6,6 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./project-thumbnail.component.css']
 })
 export class ProjectThumbnailComponent implements OnInit {
+
+  @Input("picture") picture="";
 
   constructor() {
   }
