33b263e583c9bac0386e2ee909787bfc564c0e87
Oskar <reoskaro434@gmail.com>
2021-09-25 20:15:39 +0200

    Added sing-up route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {PerfectProjectsComponent} from "./perfect-projects/perfect-projects.component";
+import {ProjectListComponent} from "./perfect-projects/body/project-list/project-list.component";
+import {SignUpComponent} from "./perfect-projects/body/sign-up/sign-up.component";
 
 const routes: Routes = [
-  { path: '', component: PerfectProjectsComponent}
+  {
+    path: '', component: PerfectProjectsComponent, children: [
+      {path: '', component: ProjectListComponent},
+      {path: 'sign-up', component: SignUpComponent},
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
