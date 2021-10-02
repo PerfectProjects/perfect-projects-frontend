38df266c33c3e78fb07e9554c107694a09b8c46a
Oskar <reoskaro434@gmail.com>
2021-10-02 19:19:15 +0200

    Created get method for retrieving user data

---
+++ src/app/services/user-profile.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UserProfileData} from "../models/user-profile-data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserProfileService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getUser = (userId: string | null) => {
+    if (userId) {
+      return this.http.get<UserProfileData>("http://127.0.0.1:5000/get-user", {headers: {"userId": userId}});
+    }
+    return;
+  }
+}
