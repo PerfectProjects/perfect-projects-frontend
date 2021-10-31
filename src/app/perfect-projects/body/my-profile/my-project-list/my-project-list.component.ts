104bdd7ffcf17e002e4ebb113461b11b27c3062a
Oskar <reoskaro434@gmail.com>
2021-10-31 16:40:37 +0100

    Created project is now visible in the user profile

--- src/app/perfect-projects/body/my-profile/my-project-list/my-project-list.component.ts
+++ src/app/perfect-projects/body/my-profile/my-project-list/my-project-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {UserProfileData} from "../../../../models/user-profile-data";
-import {catchError} from "rxjs/operators";
 import {UserProfileRestService} from "../../../../rest/user-profile-rest.service";
+import {ProjectData} from "../../../../models/project-data";
 
 @Component({
   selector: 'app-my-project-list',
@@ -10,8 +9,13 @@ import {UserProfileRestService} from "../../../../rest/user-profile-rest.service
 })
 export class MyProjectListComponent implements OnInit {
 
-  constructor(private userProfile: UserProfileRestService) { }
+  public projects?: ProjectData[];
+
+  constructor(private userProfileRest: UserProfileRestService) { }
 
   ngOnInit(): void {
+    this.userProfileRest.getProjects().subscribe((response)=>{
+      this.projects = response.projects;
+    });
   }
 }
